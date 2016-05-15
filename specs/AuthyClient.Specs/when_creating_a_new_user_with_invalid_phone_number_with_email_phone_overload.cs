74f1629586ba7c66f036f06002c9f39454e25cd4
David Sulpy <david@sulpy.com>
2016-05-15 09:09:58 -0500

    changed some specs for user validity messages returned from Authy since they now do not contain a sentence

--- specs/AuthyClient.Specs/when_creating_a_new_user_with_invalid_phone_number_with_email_phone_overload.cs
+++ specs/AuthyClient.Specs/when_creating_a_new_user_with_invalid_phone_number_with_email_phone_overload.cs
@@ -19,7 +19,7 @@ namespace AuthyClient.Specs
                                  }
                              };
 
-        private It should_contain_the_expected_message = () => _actualException.Message.ShouldEqual("User was not valid.");
+        private It should_contain_the_expected_message = () => _actualException.Message.ShouldEqual("User was not valid");
 
         private It should_throw_an_exception_with_errors = () => ((AuthyClientException)_actualException).Errors.ShouldNotBeEmpty();
 
